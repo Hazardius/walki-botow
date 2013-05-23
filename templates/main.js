2891b57a5a51632c164e5b2834e848c99ac67a28
Hazardius <hazardiusam@gmail.com>
2013-05-23 20:16:20 +0200

    Usuniecie nr linii

--- templates/main.js
+++ templates/main.js
@@ -1,7 +1,7 @@
 function validatePass(p1, p2) {
-2	    if (p1.value != p2.value || p1.value == '' || p2.value == '') {
-3	        p2.setCustomValidity('Password incorrect');
-4	    } else {
-5	        p2.setCustomValidity('');
-6	    }
-7	}
+    if (p1.value != p2.value || p1.value == '' || p2.value == '') {
+        p2.setCustomValidity('Password incorrect');
+    } else {
+        p2.setCustomValidity('');
+    }
+}
