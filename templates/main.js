bf7d55ce799d7141e9a78270efb035182e0aaa79
Hazardius <hazardiusam@gmail.com>
2013-05-24 12:49:33 +0200

    Register complete.

--- templates/main.js
+++ templates/main.js
@@ -1,7 +1,15 @@
 function validatePass(p1, p2) {
-    if (p1.value != p2.value || p1.value == '' || p2.value == '') {
-        p2.setCustomValidity('Password incorrect');
+    if (p1.value != p2.value || p1.value.length < 4 || p2.value.length < 4) {
+        p2.setCustomValidity('Password incorrect!');
     } else {
         p2.setCustomValidity('');
+    }
+}
+
+function validateLength(p1) {
+    if (p1.value.length < 4) {
+        p1.setCustomValidity('Legth too small!');
+    } else {
+        p1.setCustomValidity('');
     }
 }
