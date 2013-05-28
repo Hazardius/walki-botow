bf8e70576b03773627aded357c6084643659989e
Hazardius <s362625@pc16e.labs.wmi.amu.edu.pl>
2013-05-28 08:56:08 +0200

    'Results2'

--- botsBattles.py
+++ botsBattles.py
@@ -526,7 +526,7 @@ def register_battle(login1, login2, gameName, gameId):
 def view_battle(number, game):
     if check_ws() is False:
         return ws_error()
-    if numer == 1:
+    if number == 1:
         return render_template('results.html', username=session['username'],
             cMessages=check_messages(), number=number, game=game)
     #error = None
